963268cfd13be563617aac120d6137d39e9797ee
Nurcholis Koem <nurcholis.a.koem@gmail.com>
2025-09-22 11:01:10 +0700

    update22Sep2025

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,38 +15,45 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'admin phpt',
-            'level' => '1',
-            'username' => 'admin phpt',
-            'password' => Hash::make('123456789'),
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'admin phpt',
+        //     'level' => '1',
+        //     'username' => 'admin phpt',
+        //     'password' => Hash::make('123456789'),
+        // ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'admin arsip dan warkah',
-            'level' => '2',
-            'username' => 'admin warkah',
-            'password' => Hash::make('123456789'),
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'admin arsip dan warkah',
+        //     'level' => '2',
+        //     'username' => 'admin warkah',
+        //     'password' => Hash::make('123456789'),
+        // ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'loket 1',
-            'level' => '1',
-            'username' => 'loket 1',
-            'password' => Hash::make('123456789'),
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'loket 1',
+        //     'level' => '1',
+        //     'username' => 'loket 1',
+        //     'password' => Hash::make('123456789'),
+        // ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'loket 2',
-            'level' => '1',
-            'username' => 'loket 2',
-            'password' => Hash::make('123456789'),
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'loket 2',
+        //     'level' => '1',
+        //     'username' => 'loket 2',
+        //     'password' => Hash::make('123456789'),
+        // ]);
 
-        \App\Models\User::factory()->create([
-            'name' => 'Admin 2',
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Admin 2',
+        //     'level' => '1',
+        //     'username' => 'admin 2',
+        //     'password' => Hash::make('123456789'),
+        // ]);
+
+         \App\Models\User::factory()->create([
+            'name' => 'loket 3',
             'level' => '1',
-            'username' => 'admin 2',
+            'username' => 'loket 3',
             'password' => Hash::make('123456789'),
         ]);
 
